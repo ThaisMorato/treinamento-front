793cad6f15d0f8babbcf1c5cc25eff717abcf60e
Thais Avila Morato <thais.morato@bh.concert.com.br>
2020-08-11 17:11:20 -0300

    inicio do form para adicionar tarefa

--- thais/todo/src/app/app.component.ts
+++ thais/todo/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public title: String = 'Minhas Tarefas';
+  public todos: Todo[] = [];
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    
+    this.form = this.fb.group({
+      title: ['',Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+    
+    this.todos.push(new Todo(1,'Passear com o cachorro',false));
+    this.todos.push(new Todo(2,'Ir ao supermercado',false));
+    this.todos.push(new Todo(3,'Ir ao supermercado',true));
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index,1);
+    }
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
+
 }
