25ce15cb56ed5648e9de0dea796b0aada023fff0
Fagner Alves <altino.fagner@gmail.com>
2020-08-17 17:13:42 -0300

    projeto com bootstrap e backend json

--- Fagner/angular/src/app/app.component.ts
+++ Fagner/angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Component } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core'; 
 import { Todo } from 'src/models/todo.model';
 import { from, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TODO } from 'src/models/todo.interface'
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,18 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public tarefa: TODO = {
+    //id: 1,
+    todo: "",
+    done: false
+  }
+  public Tarefas: TODO[] = []
+
   public mode:string = 'list'
   public todos: Todo[] = []; //array vazio
   public title: string ='Minhas tarefas'
   public form: FormGroup
-  baseUrl: "http://localhost:3001/tarefas"
+  public baseUrl = "http://localhost:3001/tarefas"
 
   constructor(private fb: FormBuilder, private http: HttpClient) {
     this.form = this.fb.group({
@@ -25,73 +33,87 @@ export class AppComponent {
         Validators.required,
       ])]
     })
-    this.load()   //Dentro do construtor o método vai ser iniciado toda vez que a plicação inicia
-  }
-/*
-  createTodo ():void {
-    this.
-  }
-*/
-
-  create (todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(this.baseUrl, todo)
-    this.save()
-    this.clear()
+    this.loadRead()   //Dentro do construtor o método vai ser iniciado toda vez que a plicação inicia
   }
 
-  add() {
-    // this.form.value => { title: 'Título' }
-    const title = this.form.controls['title'].value
-    const id = this.todos.length + 1
-    this.todos.push(new Todo(id, title, false))
-    this.create(this.todos[id])
-    this.save()
-    this.clear()
+  create (teste: TODO): Observable<TODO> {
+    return this.http.post<TODO>(this.baseUrl, teste)
   }
 
-  clear() {
-    this.form.reset();
+  read (): Observable<TODO[]> {
+    return this.http.get<TODO[]>(this.baseUrl)
   }
 
+  markAsDone(tarefa: TODO) {
+    tarefa.done = true
+    return this.http.put<TODO>(`${this.baseUrl}/${tarefa.id}`, tarefa).subscribe()
+    this.loadRead()
+  }
 
-  remove(todo:Todo) {
+  markAsUndone(tarefa: TODO){
+    tarefa.done = false
+    return this.http.put<TODO>(`${this.baseUrl}/${tarefa.id}`, tarefa).subscribe()
+    this.loadRead()
+  }
+
+  remove(tarefa:TODO) {
+    this.delete(tarefa).subscribe()
+    this.loadRead()
+  }
+  delete (tarefa:TODO): Observable<TODO>{
+    return this.http.delete<TODO>(`${this.baseUrl}/${tarefa.id}`)
+  }
+  /*
     const index = this.todos.indexOf(todo);
     if (index !== -1) {
       this.todos.splice(index, 1)
-    this.save()
+    this.loadRead()
     }
+  */
+
+
+  add() {
+    const title = this.form.controls['title'].value
+    const id = this.todos.length + 1
+    this.todos.push(new Todo(id, title, false))
+    this.tarefa.todo = title
+    this.tarefa.done = false
+    this.create(this.tarefa).subscribe()
+    this.loadRead()
+    this.form.reset();
   }
 
-  markAsDone(todo: Todo) {
-    todo.done = true
-    this.save()
-  }
-
-  markAsUndone(todo: Todo){
-    todo.done = false
-    this.save()
-  }
-
-  save() {
-    const data = JSON.stringify(this.todos)
-    localStorage.setItem('todos', data)
+  loadRead() {
+    this.read().subscribe(Tarefas => {
+      this.Tarefas = Tarefas
+    })
     this.mode = 'list'
-  }
+   }
 
-  load() {
-    const data = localStorage.getItem('todos')
-    if (data) {
-      this.todos = JSON.parse(data)
-    }
-    else {
-      this.todos = []
-    }
-  }
   changeMode (mode:string) {
     this.mode = mode
   }
 
 }
+
+
+  /*
+  const data = localStorage.getItem('todos')
+  if (data) {
+    this.todos = JSON.parse(data)
+  }
+  else {
+    this.todos = []
+  }
+  */
+
+  /*
+  save() {
+    const data = JSON.stringify(this.todos)
+    localStorage.setItem('todos', data)
+    this.mode = 'list'
+  }
+  */
 
 //No typeScript com this. tenho acesso a todos os métodos e atributos dentro da classe
 //O tipo any aceita qualquer tipo de variável (number, string, etc)
--- Fagner/angular/src/app/components/navbar/navbar.component.ts
+++ Fagner/angular/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/models/todo.model'
 import { from } from 'rxjs';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-navbar',
@@ -8,11 +9,16 @@ import { from } from 'rxjs';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public Href: string = window.location.href
   public mode:string = 'list'
 
-  constructor() { }
+  constructor(private Mode: AppComponent) { }
 
   ngOnInit(): void {
-  }
+    if (this.Href == 'http://localhost:4200/change') {
+      this.Mode.mode = 'add'
+    }
+  } 
 }
+
 
